8e1b2c10bb71e024ffd3280a81b43b34776f4390
Samaelish <semnd007@gmail.com>
2021-03-19 20:07:39 +0300

    learning react

---
+++ react-task-tracker/src/components/Header.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+
+const Header = ({ title }) => {
+    return (
+        <header class='header'>
+            <h1>{title}</h1>
+            <button>Add</button>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string,
+}
+
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red', 
+//     backgroundColor: 'black'
+// }
+
+export default Header
